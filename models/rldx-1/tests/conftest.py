fd9f84643cfbffb1ac1bfff756e308ced0cb1a00
Kavya Ravi Shankar <58096384+maxmb1@users.noreply.github.com>
2026-08-23 18:27:06 -0700

    Keep the cookbook client-only
    Signed-off-by: Kavya Ravi Shankar <58096384+maxmb1@users.noreply.github.com>

--- models/rldx-1/tests/conftest.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2026 Reactor Technologies, Inc. All rights reserved.
-import os
-import sys
-
-# Make the model workspace importable (mirrors the flat imports the
-# container uses: the model folder is the working directory).
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
